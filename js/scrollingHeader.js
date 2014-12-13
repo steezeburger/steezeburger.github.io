3904b4d2ebfb7b5d45f1fd9f60c87b323cba88ea
steezeburger <jessetsnyder@gmail.com>
2014-12-13 15:38:28 -0600

    changed window scroll trigger distance from 300px to 100px

--- js/scrollingHeader.js
+++ js/scrollingHeader.js
@@ -1,7 +1,7 @@
 function init() {
   window.addEventListener('scroll', function (e) {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 300,
+      shrinkOn = 100,
       header = document.querySelector("header");
     if (distanceY > shrinkOn) {
       classie.add(header, "smaller");
