8aac164893f0d4372c4e1703a7df30ac1f9ae064
steezeburger <jessetsnyder@gmail.com>
2015-03-27 21:59:44 -0500

    added CNAME

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-  // Fullpage init  
-//  $('#fullpage').fullpage({
-//    autoScrolling: false,
-//    verticalCentered: true,
-//    resize: true,
-//    normalScrollElements: '.secondary'
-//  });  
+
 
 });
